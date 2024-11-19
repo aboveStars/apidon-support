7227dbab1494943753c3b952f92da59c51e5466e
aboveStars <yunuskorkmazis384@outlook.com>
2024-11-19 15:03:37 +0300

    minor changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+type Props = {};
+
+/** 
+ * 
+ *     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -96,6 +99,38 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+
+ * 
+ * */
+
+export default function page({}: Props) {
+  return (
+    <div className="flex w-screen h-screen justify-center content-center items-center text-center flex-col gap-8 bg-white dark:bg-black p-14">
+      <text className="text-4xl font-bold text-black dark:text-white">
+        Apidon Support
+      </text>
+      <text className="text-black dark:text-white">
+        Select a button below to get help with the topic you need assistance
+        with.
+      </text>
+      <div className="flex flex-row gap-5 items-center content-center justify-center">
+        <a
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          href="mailto:support@apidon.com?subject=Help%20Request&body=Please%20describe%20your%20issue."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Us
+        </a>
+        <a
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+          href="/privacy-policy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Privacy Policy
+        </a>
+      </div>
     </div>
   );
 }
