4e492c4720cfd1de38201bf0f9843da857ba90a0
aboveStars <yunuskorkmazis384@outlook.com>
2024-11-19 15:12:13 +0300

    minor changes.

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,4 +1,35 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Apidon • Support",
+  description: "Make Every Event Unique",
+  icons: [
+    {
+      media: "(prefers-color-scheme: light)",
+      url: "/images/android-icon-48x48.png",
+      href: "/images/android-icon-48x48.png",
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      url: "/images/android-icon-48x48.png",
+      href: "/images/android-icon-48x48.png",
+    },
+  ],
+  openGraph: {
+    title: "Apidon • Make Every Event Unique",
+    description: "Make Every Event Unique",
+    url: "https://apidon.com",
+    siteName: "Apidon",
+    images: [
+      {
+        url: "https://apidon.com/images/og.png",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+};
 
 const PrivacyPolicy = () => {
   return (
