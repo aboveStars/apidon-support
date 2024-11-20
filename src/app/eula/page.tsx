abded52eb8547c884798ba9a21f417c075cac07a
aboveStars <yunuskorkmazis384@outlook.com>
2024-11-20 12:15:35 +0300

    minor changes.

--- src/app/eula/page.tsx
+++ src/app/eula/page.tsx
@@ -1,7 +1,38 @@
 "use client";
 
+import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Apidon • Support",
+  description: "Make Every Event Unique",
+  icons: [
+    {
+      media: "(prefers-color-scheme: light)",
+      url: "/images/android-icon-48x48.png",
+      href: "/images/android-icon-48x48.png",
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      url: "/images/android-icon-48x48.png",
+      href: "/images/android-icon-48x48.png",
+    },
+  ],
+  openGraph: {
+    title: "Apidon • Make Every Event Unique",
+    description: "Make Every Event Unique",
+    url: "https://apidon.com",
+    siteName: "Apidon",
+    images: [
+      {
+        url: "https://apidon.com/images/og.png",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+};
 
 export default function page() {
   return (
